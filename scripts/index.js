a5c9782d392f92b683996aa3aba4f3f5a4236771
Aram Elchyan <elch.yan@yahoo.com>
2016-12-15 21:37:02 +0400

    First commit

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+//Binding jQuery to window
+(function() {
+    window.$ = window.jQuery = require('jquery');
+    const bookLike = require('../lib/bookLikePdf.js');
+
+    var path = undefined;
+    $(document).ready( function() {
+        $('#pdfInput').change(function(ev) {
+            path = ev.target.files[0].path;
+            
+            if((/\.(pdf)$/i).test(path)) {
+                $('#fileNameInput').val(path);
+                $('#btnConvert').prop('disabled', false);
+            } else {
+                alert("File format must be pdf");
+            }
+        });
+    });
+
+    $('#btnConvert').on('click', function() {
+        bookLike(path, () => alert('Yooho your file has been converted!') );
+    });
+})();
