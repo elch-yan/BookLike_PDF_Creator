dd004d8db71381b6c82d6e6e4833f8c56cd74b60
Aram Elchyan <elch.yan@yahoo.com>
2016-12-18 21:45:10 +0400

    Improvement: selection of number of sheets in pile added.

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,22 @@
     const bookLike = require('../lib/bookLikePdf.js');
 
     var path = undefined;
+    var sheetsInPile = 1;
+
     $(document).ready( function() {
-        $('#pdfInput').change(function(ev) {
+        var slctSheetsInPile = $('#slctSheetsInPile');
+        
+        for(let i = 1; i < 12; i++) {
+            slctSheetsInPile.append($("<option></option>")
+                    .attr("value", i)
+                    .text(i));
+        }
+
+        slctSheetsInPile.on('change', function() {
+            sheetsInPile = parseInt($(this).val());
+        });
+        
+        $('#pdfInput').on('change', function(ev) {
             path = ev.target.files[0].path;
             
             if((/\.(pdf)$/i).test(path)) {
@@ -18,7 +32,7 @@
     });
 
     $('#btnConvert').on('click', function() {
-        bookLike(path, (err) => {
+        bookLike(path, sheetsInPile, (err) => {
             if(err) {
                 return alert(err);
             }
